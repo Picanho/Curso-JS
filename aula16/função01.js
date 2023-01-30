8f1387161401195ad3cdd9fe7c01a837ea16e29f
Lucas <116681825+Picanho@users.noreply.github.com>
2023-01-30 20:43:20 -0300

    Add files via upload

---
+++ aula16/função01.js
@@ -0,0 +1,9 @@
+function parimpar(n) {
+  if (n % 2 == 0) {
+    return "par";
+  } else {
+   return "impar";
+  }
+}
+let res = parimpar(15); //console.log(parimpar(4)) outra maneira abreviada de fazer e sem var
+console.log(res)
---
+++ aula16/função02.js
@@ -0,0 +1,5 @@
+function soma(n1=0,n2=0) {
+    return n1 + n2 
+}
+
+console.log(soma(7,3)) //caso so tenha um numero dentro dos parenteses dará NaN, a menos que coloque n1=0 n2=0
